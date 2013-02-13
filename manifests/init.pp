155ee4827a1a8623de16be4c59ff99e9e63d9dea
Tom Wardill <tom@howrandom.net>
2013-02-13 16:19:20 +0000

    Install the sd-agent package if the host system is Ubuntu

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,34 @@
-class puppet-serverdensity( $content ) {
+class apt {
+
+    file {
+        'sd-agent.list':
+            path    => '/etc/apt/sources.list.d/sd-agent.list',
+            ensure  =>  file,
+            content => 'deb http://www.serverdensity.com/downloads/linux/deb all main',
+    }
+
+    exec {
+        'add-sd-apt-key':
+            command => '/usr/bin/wget -O - https://www.serverdensity.com/downloads/boxedice-public.key | /usr/bin/apt-key add -',
+            require => File['sd-agent.list'],
+    }
+
+    exec {
+        'sd-apt-update':
+            command => '/usr/bin/apt-get update',
+            require => Exec['add-sd-apt-key'],
+    }
+
+    package {
+        'sd-agent':
+            ensure => 'present',
+            require => Exec['sd-apt-update']
+    }
     
-    file { 'test':
-        path => '/tmp/test_file',
-        content => $content
+}
+
+class puppet-serverdensity( $content ) {
+    case $::operatingsystem {
+        'Ubuntu': { include apt }
     }
 }
