3380f23ee31ceb7dd34f197924e06012f34e5bb3
Pedro Pessoa <pedropessoa@boxedice.com>
2013-02-28 17:33:56 +0000

    almost all of puppet-lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# == Class: apt
+# == Class: serverdensity
 #
 # Full description of class example_class here.
 #
@@ -35,173 +35,81 @@
 #
 # Copyright 2011 Your name here, unless otherwise noted.
 #
-class apt {
-
-    file {
-        'sd-agent.list':
-            ensure  =>  file,
-            path    => '/etc/apt/sources.list.d/sd-agent.list',
-            source  => 'puppet:///modules/puppet-serverdensity/sd-agent.list',
-            notify  => Exec['sd-apt-update']
+class serverdensity(
+  $sd_url = 'https://example.serverdensity.com',
+  $api_token = '',
+  $api_username = '',
+  $api_password = '',
+  $agent_key = '',
+  $server_name = '',
+  $plugin_directory = '',
+  $apache_status_url = 'http://www.example.com/server-status/?auto',
+  $apache_status_user = '',
+  $apache_status_pass = '',
+  $mongodb_server = '',
+  $mongodb_dbstats = 'no',
+  $mongodb_replset = 'no',
+  $mysql_server = '',
+  $mysql_user = '',
+  $mysql_pass = '',
+  $nginx_status_url = 'http://www.example.com/nginx_status',
+  $rabbitmq_status_url = 'http://www.example.com:55672/json',
+  $rabbitmq_user = '',
+  $rabbitmq_pass = '',
+  $tmp_directory = '',
+  $pidfile_directory = '',
+  $logging_level = '',
+  ) {
+  case $::operatingsystem {
+    'Ubuntu': {
+      include apt
+        $location = '/etc/sd-agent/config.cfg'
     }
-
-    exec {
-        'add-sd-apt-key':
-            command => '/usr/bin/wget -O - https://www.serverdensity.com/downloads/boxedice-public.key | /usr/bin/apt-key add -',
-            require => File['sd-agent.list'],
+    'CentOS': {
+      include yum
+        $location = '/etc/sd-agent/config.cfg'
     }
-
-    exec {
-        'sd-apt-update':
-            command => '/usr/bin/apt-get update',
-            require => Exec['add-sd-apt-key'],
-    } 
-
-    package {
-        'sd-agent':
-            ensure  => 'present',
-            require => Exec['sd-apt-update'],
-    }    
-}
-
-class sd_yum {
-
-    file {
-        'sd-agent.repo':
-            ensure  =>  file,
-            path    => '/etc/yum.repos.d/serverdensity.repo',
-            content => '[serverdensity]
-name=Server Density
-baseurl=http://www.serverdensity.com/downloads/linux/redhat/
-enabled=1',
+    default: {
+      fail("OSfamily ${::operatingsystem} not supported.")
     }
+  }
 
-    package {
-        'wget':
-            ensure => 'present',
-    }
-
-    exec {
-        'download-sd-yum-key':
-            command => '/usr/bin/wget https://www.serverdensity.com/downloads/boxedice-public.key',
-            require => [File['sd-agent.repo'], Package['wget']],
-    }
-
-    exec {
-        'import-sd-yum-key':
-            command => '/usr/bin/sudo rpm --import boxedice-public.key',
-            require => Exec['download-sd-yum-key'],
-    }
-
-    exec {
-        'delete-sd-yum-key':
-            command => '/bin/rm boxedice-public.key',
-            require => Exec['import-sd-yum-key'],
-    }
-
-    package {
-        'sd-agent':
-            ensure => 'present',
-            require => Exec['delete-sd-yum-key']
-    } 
-}
-
-class sd_config_file ( 
-        $location,
+  class {
+    'config_file':
+      location            => $location,
+      require             => Package['sd-agent'],
+      sd_url              => $sd_url,
+      agent_key           => agent_key(
+        $api_username,
+        $api_password,
         $sd_url,
+        $api_token,
         $agent_key,
-        $plugin_directory,
-        $apache_status_url,
-        $apache_status_user,
-        $apache_status_pass,
-        $mongodb_server,
-        $mongodb_dbstats,
-        $mongodb_replset,
-        $mysql_server,
-        $mysql_user,
-        $mysql_pass,
-        $nginx_status_url,
-        $rabbitmq_status_url,
-        $rabbitmq_user,
-        $rabbitmq_pass,
-        $tmp_directory,
-        $pidfile_directory,
-        $logging_level
-    ) {
-    file { 'sd-agent-config-file':
-        path => $location,
-        content => template('puppet-serverdensity/config.template'),
-        
-    }
-}
+        $server_name
+        ),
+      plugin_directory    => $plugin_directory,
+      apache_status_url   => $apache_status_url,
+      apache_status_user  => $apache_status_user,
+      apache_status_pass  => $apache_status_pass,
+      mongodb_server      => $mongodb_server,
+      mongodb_dbstats     => $mongodb_dbstats,
+      mongodb_replset     => $mongodb_replset,
+      mysql_server        => $mysql_server,
+      mysql_user          => $mysql_user,
+      mysql_pass          => $mysql_pass,
+      nginx_status_url    => $nginx_status_url,
+      rabbitmq_status_url => $rabbitmq_status_url,
+      rabbitmq_user       => $rabbitmq_user,
+      rabbitmq_pass       => $rabbitmq_pass,
+      tmp_directory       => $tmp_directory,
+      pidfile_directory   => $pidfile_directory,
+      logging_level       => $logging_level,
+      notify              => Service['sd-agent']
+  }
 
-class serverdensity( 
-    $sd_url,
-    $api_token = '',
-    $api_username = '',
-    $api_password = '',
-    $agent_key = '',
-    $server_name = '',
-    $plugin_directory = '',
-    $apache_status_url = 'http://www.example.com/server-status/?auto',
-    $apache_status_user = '',
-    $apache_status_pass = '',
-    $mongodb_server = '',
-    $mongodb_dbstats = 'no',
-    $mongodb_replset = 'no',
-    $mysql_server = '',
-    $mysql_user = '',
-    $mysql_pass = '',
-    $nginx_status_url = 'http://www.example.com/nginx_status',
-    $rabbitmq_status_url = 'http://www.example.com:55672/json',
-    $rabbitmq_user = '',
-    $rabbitmq_pass = '',
-    $tmp_directory = '',
-    $pidfile_directory = '',
-    $logging_level = ''
-) {
-
-    case $::operatingsystem {
-        'Ubuntu': { 
-            include sd-apt
-            $location = '/etc/sd-agent/config.cfg'
-        }
-        'CentOS': { 
-            include sd-yum 
-            $location = '/etc/sd-agent/config.cfg'
-        }
-    }
-
-    class {
-           'sd-config-file':
-                location => $location,
-                require => Package['sd-agent'],
-                sd_url => $sd_url,
-                agent_key => agent_key($api_username, $api_password, $sd_url, $api_token, $agent_key, $server_name),
-                plugin_directory => $plugin_directory,
-                apache_status_url => $apache_status_url,
-                apache_status_user => $apache_status_user,
-                apache_status_pass => $apache_status_pass,
-                mongodb_server => $mongodb_server,
-                mongodb_dbstats => $mongodb_dbstats,
-                mongodb_replset => $mongodb_replset,
-                mysql_server => $mysql_server,
-                mysql_user => $mysql_user,
-                mysql_pass => $mysql_pass,
-                nginx_status_url => $nginx_status_url,
-                rabbitmq_status_url => $rabbitmq_status_url,
-                rabbitmq_user => $rabbitmq_user,
-                rabbitmq_pass => $rabbitmq_pass,
-                tmp_directory => $tmp_directory,
-                pidfile_directory => $pidfile_directory,
-                logging_level => $logging_level,
-
-                notify => Service['sd-agent']
-    }
-
-    service {
-        'sd-agent':
-            ensure  => 'running',
-            enable  => true,
-    }
+  service {
+    'sd-agent':
+      ensure  => 'running',
+      enable  => true,
+  }
 }
