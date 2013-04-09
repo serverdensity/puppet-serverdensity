bae7b6406e5ee713f557e0806aec4d5078ae06e4
Tom Wardill <tom@howrandom.net>
2013-04-09 17:36:56 +0100

    Add explanation comments

--- lib/facter/agent_key.rb
+++ lib/facter/agent_key.rb
@@ -4,11 +4,18 @@ require 'uri'
 
 Facter.add(:agent_key) do
 
+    # just in case we don't get any of them
     result = nil
 
+    # We inject this file using the Rackspace api
+    # on instance creation
+    # do this first as it's fast
     if File::exist?('/etc/sd-agent-key')
         result = Facter::Util::Resolution.exec("cat /etc/sd-agent-key")
     else
+        # use the amazon metadata api to
+        # get user-data that we've set on
+        # instance creation
         uri = URI("http://169.254.169.254/latest/user-data")
         req = Net::HTTP::Get.new(uri.request_uri)
         res = Net::HTTP.start(uri.host, uri.port) {|http|
@@ -17,5 +24,11 @@ Facter.add(:agent_key) do
 
         result = res.body.split(':').last
     end
+
+    # if we get to here and neither of the above
+    # methods have worked
+    # the custom function will use the api to create
+    # a new device, rather than matching to an existing one
+
     setcode { result }
 end
