e4ab5b95fb1269e4d29cb0161c62a0a33a9e7187
David Mytton <david@mytton.net>
2014-03-25 11:21:12 +0000

    All indents to spaces

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -1,18 +1,18 @@
 class serverdensity-agent::apt {
-	$repo_baseurl = 'http://www.serverdensity.com/downloads/linux/deb'
-	$repo_keyurl = 'https://www.serverdensity.com/downloads/boxedice-public.key'
+    $repo_baseurl = 'http://www.serverdensity.com/downloads/linux/deb'
+    $repo_keyurl = 'https://www.serverdensity.com/downloads/boxedice-public.key'
 
-	apt::source { 'serverdensity-agent':
-		location    => $repo_baseurl,
-		release     => 'all',
-		repos       => 'main',
-		key         => '13C2E6F8',
-		key_source  => $repo_keyurl,
-		include_src => false
-	}
-	package {
-			'sd-agent':
-					ensure  => 'present',
-					require => Apt::Source['serverdensity-agent'],
-	}
+    apt::source { 'serverdensity-agent':
+        location    => $repo_baseurl,
+        release     => 'all',
+        repos       => 'main',
+        key         => '13C2E6F8',
+        key_source  => $repo_keyurl,
+        include_src => false
+    }
+    package {
+            'sd-agent':
+                    ensure  => 'present',
+                    require => Apt::Source['serverdensity-agent'],
+    }
 }
