34772f216e520329f65b8484ea975428f94c4de9
Vik Bhatti <vik@vikbhatti.com>
2014-03-25 13:36:16 +0000

    Added param docs to main manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,134 @@
 # == Class: serverdensity
 #
-# Full description of class example_class here.
+# Base serverdensity class
 #
 # === Parameters
 #
-# Document parameters here.
+# [*sd_url*]
+#   String. Subdomain url of the serverdensity account
+#   Default: https://example.serverdensity.com
 #
-# [*ntp_servers*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*api_token*]
+#   String. Agent API token to use (for V2 API)
+#   Default: ''
 #
-# === Variables
+# [*api_username*]
+#   String. Agent API username to use (for V1 API)
+#   Default: ''
 #
-# Here you should define a list of variables that this module would require.
+# [*api_password*]
+#   String: Agent API password to use (for V1 API)
+#   Default: ''
 #
-# [*enc_ntp_servers*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# [*use_fqdn*]
+#   Boolean. Use the puppet FQDN fact rather than hostname
+#   Default: false
+#   Valid values: true, false
+#
+# [*agent_key*]
+#   String. The unique key for the agent.
+#   Default: $::agent_key (uses the provided agent_key library to generate a
+#   new one if not set)
+#
+# [*server_name*]
+#   String. The reported name of the server
+#   Default: ''
+#
+# [*server_group*]
+#   String. The server group to assign this host to
+#   Default: ''
+#
+# [*plugin_directory*]
+#   String. The directory to install agent plugins to
+#   Default: /usr/bin/sd-agent/plugins
+#
+# [*apache_status_url*]
+#   String. Url to the apache status page provided by the mod_status module
+#   Default: http://www.example.com/server-status/?auto
+#
+# [*apache_status_user*]
+#   String. Username required to access apache status URL
+#   Default: ''
+#
+# [*apache_status_pass*]
+#   String. Password required to access apache status URL
+#   Default: ''
+#
+# [*fpm_status_url*]
+#   String. URL to the phpfpm status page
+#   Default: http://www.example.com/phpfpm_status
+#
+# [*mongodb_server*]
+#   String. Server to get MongoDB status monitoring from. Takes a full MongoDB connection URI
+#   Default: ''
+#
+# [*mongodb_dbstats*]
+#   String. Enable MongoDB stats monitoring (only if $mongodb_server is also set)
+#   Default: 'no'
+#
+# [*mongodb_replset*]
+#   String. Enable MongoDB replica monitoring (only if $mongodb_server is also set)
+#   Default: 'no'
+#
+# [*mysql_server*]
+#   String. MySQL server to get status monitoring from
+#   Default: ''
+#
+# [*mysql_user*]
+#   String. Username to access MySQL server
+#   Default: ''
+#
+# [*mysql_pass*]
+#   String. Password required to access MySQL server
+#   Default: ''
+#
+# [*nginx_status_url*]
+#   String. URL to get nginx status (using HttpStubStatusModule)
+#   Default: http://www.example.com/nginx_status
+#
+# [*rabbitmq_status_url*]
+#   String. URL to rabbitmq status endpoint
+#   Default: http://www.example.com:55672/json
+#
+# [*rabbitmq_user*]
+#   String. Username required to access RabbitMQ status
+#   Default: ''
+#
+# [*rabbitmq_pass*]
+#   String. Password required to access RabbitMQ status
+#   Default: ''
+#
+# [*tmp_directory*]
+#   String. Directory where the agent stores temporary files. Defaults to system tmp
+#   Default: ''
+#
+# [*pidfile_directory*]
+#   String. Directory where agent stores its PID file. Defaults to $tmp_directory or system temp
+#   Default: ''
+#
+# [*logging_level*]
+#   String. Logging level to use for agent. Defaults to info if not set
+#   Default: ''
+#   Valid values: debug, info, warn, error, fatal
+#
 #
 # === Examples
 #
-#  class { 'example_class':
-#    ntp_servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  V2 API
+#
+#  class { 'serverdensity':
+#    sd_url     => 'https://example.serverdensity.io',
+#    api_token  => 'APITOKENHERE',
 #  }
+#
 #
 # === Authors
 #
-# Author Name <author@example.com>
+# David Mytton <david@serverdensity.com>
 #
 # === Copyright
 #
-# Copyright 2011 Your name here, unless otherwise noted.
+# Copyright 2011 Server Density
 #
 class serverdensity(
   $sd_url = 'https://example.serverdensity.com',
