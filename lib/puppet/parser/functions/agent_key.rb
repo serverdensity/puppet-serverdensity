1d6dd8a1e1c6a520b17d9a56ee225df73cadd361
Jordi Soucheiron <jordi@serverdensity.com>
2014-05-12 18:03:09 +0200

    Better support for Google Cloud instances

--- lib/puppet/parser/functions/agent_key.rb
+++ lib/puppet/parser/functions/agent_key.rb
@@ -150,14 +150,26 @@ module Puppet::Parser::Functions
             # attempt to find the device by hostname (which may be local or FQDN)
             list = nil
             checks.each do |hn|
-                filter = {
-                    'type' => 'device',
-                    'hostname' => hn,
-                }
+                hn_split=hn.split(".")
+                # attempt to detect google cloud devices
+                if hn.end_with?(".internal") and hn_split.length >=4 and hn_split[-3] == 'c'
+                    projectId=hn_split[-2]
+                    name=hn_split[0..-4].join('.')
+                    filter = {
+                        'type' => 'device',
+                        'name' => name,
+                        'projectId' => projectId,
+                    }
+                else
+                    filter = {
+                        'type' => 'device',
+                        'hostname' => hn,
+                    }
+                end
 
                 filter_json = URI.escape(PSON.dump(filter))
 
-                notice ["Making API request"]
+                notice ["Making API request for hostname: #{ hn }"]
 
                 begin
                     uri = URI("#{ base_url }/inventory/devices?filter=#{ filter_json }&token=#{ token }")
