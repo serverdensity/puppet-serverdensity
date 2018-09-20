552c0487805a0d742cb88843842c11ceae834b3c
shurl <shurl@users.noreply.github.com>
2018-09-20 15:35:53 +0100

    add bionic support

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -14,6 +14,7 @@
 class serverdensity_agent::apt {
   $repo_location = $::lsbdistcodename ? {
     'artful'  => 'xenial',
+    'bionic'  => 'bionic',
     'precise' => 'all',
     'quantal' => 'all',
     'raring'  => 'all',
